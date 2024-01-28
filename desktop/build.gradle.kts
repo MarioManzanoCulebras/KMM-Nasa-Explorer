cb244b094c52141991019455355007572c369916
Mario Manzano <bross1112@gmail.com>
2024-01-27 19:01:08 -0800

    Added windows options to create distributable.

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
 }
 
 group = "com.mariomanzano.kmm_nasa_explorer"
-version = "1.0-SNAPSHOT"
 
 kotlin {
     jvm {
@@ -29,13 +28,27 @@ compose.desktop {
     application {
         javaHome = "/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home"
         nativeDistributions {
-            packageVersion = "1.0-SNAPSHOT"
+            packageVersion = "1.0.0"
             outputBaseDir.set(project.buildDir.resolve("OutputDir"))
             includeAllModules = true
+            windows {
+                // a version for all Windows distributables
+                packageVersion = "1.0.0"
+                // a version only for the msi package
+                msiPackageVersion = "1.0.0"
+                // a version only for the exe package
+                exePackageVersion = "1.0.0"
+            }
         }
         mainClass = "com.mariomanzano.kmm_nasa_explorer.MainKt"
         nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            targetFormats(
+                TargetFormat.Dmg,
+                TargetFormat.Msi,
+                TargetFormat.Deb,
+                TargetFormat.Exe,
+                TargetFormat.Msi
+            )
             packageName = "KMM-Nasa-Explorer"
             packageVersion = "1.0.0"
         }
